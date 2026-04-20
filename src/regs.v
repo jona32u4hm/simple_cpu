461d8c01f812bce309a357de5eee10ae0815e51e
Jonatan HidalgoMorales <jona32u4hm@gmail.com>
2026-04-20 17:52:12 -0600

    wrote register file

---
+++ src/regs.v
@@ -0,0 +1,25 @@
+module REG_FILE #(
+        parameter pADDR_WIDTH = 1,      // 2 registers (2^1)
+        parameter pDATA_WIDTH = 8
+    )(
+        input CLK, RESET, write_enable,
+        input  [pADDR_WIDTH-1:0]   addr_rs1, // Read addr 1
+        input  [pADDR_WIDTH-1:0]   addr_rs2, // Read addr 2
+        input  [pADDR_WIDTH-1:0]   addr_rd,  // Write addr
+        input  [pDATA_WIDTH-1:0]   data_in,  // Data to write
+        output [pDATA_WIDTH-1:0]   data_rs1, // Data to read 1
+        output [pDATA_WIDTH-1:0]   data_rs2  // Data to read 2
+    );
+    reg [pDATA_WIDTH-1:0] _registers [0:(1 << pADDR_WIDTH)-1];
+
+    assign data_rs1 = _registers[addr_rs1];
+    assign data_rs2 = _registers[addr_rs2];
+
+    always @(posedge CLK) begin
+        if (RESET) begin
+            
+        end else if (write_enable) begin 
+            _registers[addr_rd] <= data_in;
+        end
+    end
+endmodule
