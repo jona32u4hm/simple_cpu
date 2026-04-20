e2ccf1f2adfbf04283e09d4966fcbd4965f27e28
Jonatan HidalgoMorales <jona32u4hm@gmail.com>
2026-04-20 12:29:22 -0600

    first version of RAM

--- src/ram.v
+++ src/ram.v
@@ -1,11 +1,20 @@
-module RAM(
-    input MEM_CONTROL,
-    input [11:0] MEM_ADDR,
-    output reg  [7:0] MEM_DATA,
+module RAM #(
+    parameter pADDR_WIDTH = 12, 
+    parameter pDATA_WIDTH = 8   
+    )(
+    input   [pADDR_WIDTH-1:0] MEM_ADDR,
+    output  [pDATA_WIDTH-1:0] MEM_DATA
     );
 
+    // Memory size
+    reg [pDATA_WIDTH-1:0] _memory [0:(1 << pADDR_WIDTH)-1];
 
-
-
+    always @(*) begin 
+        if (MEM_CONTROL == 0) begin //store in memory
+            _memory[MEM_ADDR] <= MEM_DATA;
+        end else begin // load from memory
+            MEM_DATA = _memory[MEM_ADDR];
+        end
+    end
 
 endmodule
