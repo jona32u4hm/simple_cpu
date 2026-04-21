614ac4ea05ddd3b705313cc2562c3a26095a90d0
Jonatan HidalgoMorales <jona32u4hm@gmail.com>
2026-04-21 08:52:45 -0600

    completed ALU untested

--- src/alu.v
+++ src/alu.v
@@ -1,6 +1,6 @@
+`include "include/aludefines.vh"
 module ALU#(
-        parameter pOP_COUNT, //number of operations
-        parameter pALU_WIDTH = $clog2(pOP_COUNT),      // size of operation bus (2^1)
+        parameter pALU_WIDTH =  `ALU_OP_WIDTH,      // size of operation bus (2^1)
         parameter pDATA_WIDTH = 8
     )(
         input  [pDATA_WIDTH-1:0] operand_1,
@@ -10,6 +10,24 @@ module ALU#(
         output z_flag
     );
 
-    
+    // mapping:
+    localparam _ADD    = `ALU_ADD,
+               _SUB    = `ALU_SUB,
+               _AND    = `ALU_AND, 
+               _OR     = `ALU_OR, 
+               _BYPASS = `ALU_BYPASS,
+               _EQUAL  = `OP_EQUAL;
+
+    always @(*) begin
+        case (alu_op)
+            _ADD:       result = operand_1 + operand_2;
+            _SUB:       result = operand_1 - operand_2;
+            _AND:       result = operand_1 & operand_2;
+            _OR:        result = operand_1 | operand_2;
+            default: result = 0;
+        endcase
+    end
+
+    assign z_flag = (result==0)? 1 : 0;
 
 endmodule
